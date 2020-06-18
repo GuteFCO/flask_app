f10dfc730f3220e2e6ecd8486d66bcf928535d9c
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2020-06-17 21:23:57 -0400

    Utilizando métodos y respuestas http

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+class Usuario:
+    def __init__(self, nombre, password, confirmacion):
+        self.nombre = nombre
+        self.password = password
+
+
 @app.route('/')
 def index():
     return '<h1>Hola</h1>'
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
     datos = request.get_json()
 
-    print(datos['nombre'])
+    usuario = Usuario(**datos)
 
-    return '<h1>OK</h1>'
+    return {'id': 1, 'nombre': usuario.nombre}, 201
